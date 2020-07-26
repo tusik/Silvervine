1e54d08efc0c9462f9efe04bdbeabd1fd5f89719
Image <image@by.cx>
2020-07-26 15:29:21 +0800

    full open file

--- src/model/silvervinefile.cpp
+++ src/model/silvervinefile.cpp
@@ -1,5 +1,6 @@
 ﻿#include "silvervinefile.h"
-
+#include <QTextStream>
+#include <QCryptographicHash>
 Silvervine::SilvervineFile::SilvervineFile(QObject* parent) : QFile(parent)
 {
 
@@ -8,4 +9,25 @@ Silvervine::SilvervineFile::SilvervineFile(QObject* parent) : QFile(parent)
 Silvervine::SilvervineFile::SilvervineFile(const QString& name): QFile(name)
 {
 
+}
+
+bool Silvervine::SilvervineFile::fullOpen(QIODevice::OpenMode flags)
+{
+    if(open(flags))
+    {
+        QTextStream ts(this);
+        int lineNum = 0;
+        QCryptographicHash qhash(QCryptographicHash::Sha1);
+        while(!ts.atEnd())
+        {
+            qhash.addData(ts.readLine().toStdString().c_str());
+            SilvervineFileLine line;
+            line.hash = qhash.result().toLongLong();
+            line.lineNumber = lineNum;
+            qhash.reset();
+            fileLines.push_back(line);
+            lineNum++;
+        }
+    }
+    return false;
 }
--- src/model/silvervinefile.h
+++ src/model/silvervinefile.h
@@ -9,17 +9,26 @@ namespace Silvervine
 struct SilvervineFileLine
 {
     int lineNumber = 0;
-    QByteArray hash = "0";
+    qint64 hash = 0;
 };
 
 class SilvervineFile : public QFile
 {
     Q_OBJECT
+    enum FileType
+    {
+        NONE = INT_MIN,
+        Text = 1,
+        Binary = 2,
+        END = INT_MAX,
+    };
 public:
     explicit SilvervineFile(QObject* parent = nullptr);
     SilvervineFile(const QString& name);
-
-    std::map<int, SilvervineFileLine> file;
+    bool fullOpen(OpenMode flags);
+    FileType type = FileType::NONE;
+private:
+    std::vector<SilvervineFileLine> fileLines;
 signals:
 
 public slots:
