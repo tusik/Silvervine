44856b0e928a73fcbed47ccacae2b47747c1c0f9
Image <image@by.cx>
2020-08-04 21:23:18 +0800

    fix open too small file

--- src/model/silvervinefile.cpp
+++ src/model/silvervinefile.cpp
@@ -32,6 +32,10 @@ long Silvervine::SilvervineFile::isEndOfLine(char* ptr, unsigned long size)
     auto pos = memchr(ptr, '\r', size);
     if(nullptr != pos)
     {
+        if(*(static_cast<char*>(pos) + 1) == '\n')
+        {
+            return static_cast<char*>(pos) - ptr + 1 ;
+        }
         return static_cast<char*>(pos) - ptr;
     }
     else
@@ -50,14 +54,14 @@ bool Silvervine::SilvervineFile::readFileByByte()
     int size = READ_BUF_SIZE;
     int actualSize = 0;
     QByteArray line;
-    while((actualSize = ds.readRawData(buf, size)) > 0 && !ds.atEnd())
+    while((actualSize = ds.readRawData(buf, size)) > 0)
     {
         long bufPtr = 0, bufOffset = 0;
 
         while((bufOffset = isEndOfLine(buf + bufPtr, static_cast<unsigned long>(actualSize - bufOffset)))  >= 0)
         {
 
-            line.append(buf + bufPtr, static_cast<int>(bufOffset));
+            line.append(buf + bufPtr, static_cast<int>(bufOffset) + 1);
             qhash.addData(line);
             SilvervineFileLine* sfline = new SilvervineFileLine();
             if(forword != nullptr)
@@ -72,7 +76,9 @@ bool Silvervine::SilvervineFile::readFileByByte()
             fileLines.push_back(sfline);
             countSize += sfline->pos;
             bufPtr += bufOffset + 1;
+            qDebug() << line;
             line.clear();
+
         }
         line.append(buf + bufPtr, static_cast<int>(bufOffset));
     }
